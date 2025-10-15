6ffb29b1d036878f216f1f929b81f0ba35b824a0
Drew <webmaster@dhornbein.com>
2025-10-15 14:35:26 -0600

    Enable SSG mode with static preset and add runtime config for petition URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,14 +9,29 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxtjs/tailwindcss'
   ],
+  image: {
+    provider: 'none',
+    dir: 'public'
+  },
   tailwindcss: {
     configPath: 'tailwind.config.ts'
+  },
+  nitro: {
+    preset: 'static',
+    prerender: {
+      routes: ['/']
+    }
   },
   app: {
     head: {
       link: [
         { rel: 'stylesheet', href: 'https://use.typekit.net/hdp3ciu.css' }
       ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      petitionUrl: process.env.NUXT_PUBLIC_PETITION_URL || 'https://actionnetwork.org/petitions/test-petition-352?source=direct_link&'
     }
   }
 })
