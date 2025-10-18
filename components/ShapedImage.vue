8017798f5cb0c0567c50fb0f1625da25d00b63af
Drew <webmaster@dhornbein.com>
2025-10-17 18:30:57 -0600

    adds images

---
+++ components/ShapedImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtImg 
+    :src="src" 
+    :alt="alt"
+    :style="{ shapeOutside: `url('${src}')`, shapeImageThreshold: threshold }" 
+    class="shape"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string
+  alt?: string
+  threshold?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  alt: '',
+  threshold: 0.2
+})
+</script>
+
+<style scoped>
+.shape {
+  shape-image-threshold: 0.2;
+}
+</style>
